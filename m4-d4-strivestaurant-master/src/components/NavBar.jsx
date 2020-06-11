5c6f44c9e014c837e3861268119d6a8ac4bbf0d7
Alberto Gaias <63840357+albfix@users.noreply.github.com>
2020-06-11 15:28:38 +0200

    changed DishComment.jsx in functional comp + 1st try on NavBar

--- m4-d4-strivestaurant-master/src/components/NavBar.jsx
+++ m4-d4-strivestaurant-master/src/components/NavBar.jsx
@@ -2,41 +2,49 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link, withRouter } from 'react-router-dom'
 
-const NavBar = (props) => {
-  return (
-    <div>
-      {console.log('NAVBAR PROPS --> ', props)}
-      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-        <Link to="/">
-          <Navbar.Brand>
-            {props.title} - Striving For Food
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}     ///////CREATA CLASSE NAVBAR E CONSTRUCTOR, MA CONTINUARE QUA 
+  }
+  render() {
+    console.log(this.props)
+
+    return (
+      <div>
+        {console.log('NAVBAR PROPS --> ', props)}
+        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+          <Link to="/">
+            <Navbar.Brand>
+              {props.title} - Striving For Food
           </Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <Link to="/menu"
-              className={
-                props.location.pathname === '/menu'
-                  ? "nav-link active"
-                  : "nav-link"
-              }>
-              Menu
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Link to="/menu"
+                className={
+                  props.location.pathname === '/menu'
+                    ? "nav-link active"
+                    : "nav-link"
+                }>
+                Menu
             </Link>
-            <Link to="/reservation"
-              className={
-                props.location.pathname === '/reservation'
-                  ? "nav-link active"
-                  : "nav-link"
-              }>
-              Reservation
+              <Link to="/reservation"
+                className={
+                  props.location.pathname === '/reservation'
+                    ? "nav-link active"
+                    : "nav-link"
+                }>
+                Reservation
             </Link>
-            <Link to="/location" className="nav-link">Our Location</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+              <Link to="/location" className="nav-link">Our Location</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default withRouter(NavBar);
